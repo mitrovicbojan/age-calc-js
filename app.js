ed2636671a09fb34c396446c62d21baf6f1dd244
unknown <mitrovicbojan87@gmail.com>
2023-10-01 20:22:57 +0300

    label

---
+++ app.js
@@ -0,0 +1,7 @@
+const errorDay = document.getElementById("error-day");
+const errorMonth = document.getElementById("error-month");
+const errorYear = document.getElementById("error-year");
+
+// errorDay.style.display = "None";
+// errorMonth.style.display = "None";
+// errorYear.style.display = "None";
