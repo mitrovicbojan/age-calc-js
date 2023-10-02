c14e5f15cddfcb517ddccfc9acc48b49efdcb69c
unknown <mitrovicbojan87@gmail.com>
2023-10-02 21:20:02 +0300

    js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const errorDay = document.getElementById("error-day");
 const errorMonth = document.getElementById("error-month");
 const errorYear = document.getElementById("error-year");
+let dayInput = document.getElementById("day");
+let monthInput = document.getElementById("month");
+let yearInput = document.getElementById("year");
+let yearOutput = document.getElementById("year-span");
+let monthOutput = document.getElementById("month-span");
+let dayOutput = document.getElementById("day-span");
 
 errorDay.style.display = "None";
 errorMonth.style.display = "None";
 errorYear.style.display = "None";
+
+function dateCalc() {
+  let newDay = dayInput.value;
+  dayOutput.innerHTML = newDay;
+}
